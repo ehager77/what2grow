13d1c93edfe649cd3551b9a9df00e236945aa49d
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-12 16:50:59 -0700

    reading values from database, starting to implement button click listener

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ var arrAll = [];
 var startMonth;
 //the array will hold the four consecutive nearest months numbers in th future
 var fourMonths = [];
-
+var availableTags = [];
 
 //var wthrDay;
 var latitude = "37.7749";
@@ -30,9 +30,6 @@ var okPlantMinTempMonth02;
 var okPlantMinTempMonth03;
 var okPlantMinTempMonth04;
 
-
-
-
 var config = {
     apiKey: "AIzaSyB6QHUTluMJJPvnd2jh7uGY3CdHt_Mhkzk",
     authDomain: "what2grow-1df35.firebaseapp.com",
@@ -44,415 +41,308 @@ var config = {
 
 firebase.initializeApp(config);
 
-var root = firebase.database();
-var vegetables = root.ref().child('Vegetables')
+var database = firebase.database();
 
-var veggies = [{
-    name: "Tomato",
-    minTemp: 20,
-    maxTemp: 35,
-    minPh: 5.5,
-    maxPh: 6.5,
-    daysToHarvest: 80,
-    plantingMethod: {
-        seed: true,
-        cutting: true,
-        transplant: true
+var trefleKey = "QythcWY2bk1NVlVvemFRbUFCRmEyUT09";
+var veggies = [
+
+    // Cold Weather
+
+    {
+        name: "Carrot",
+        url: "http://trefle.io/api/plants/127362?token=" + trefleKey,
+        minTemp: 5,
+        optimalTemp: 22,
+        maxTemp: 30,
+        minPh: 5.5,
+        maxPh: 6.5,
+        daysToHarvest: 70,
+        plantingMethond: {
+            seed: true,
+            cutting: false,
+            transplant: false,
+        },
+        plantingDensity: {
+            greenhouse: false,
+            field: 30
+        }
+
     },
-    plantingDensity: {
-        greenhouse: 4,
-        field: 2
-    }
-},
-{
-    name: "Potato",
-    temp: "something else"
-},
-{
-    name: "Carrots",
-    temp: "something"
-},
-{
-    name: "Cabbage",
-    temp: "something"
-},
-{
-    name: "Corn",
-    temp: "something"
-}]
 
+    {
+        name: "Beets",
+        url: "http://trefle.io/api/plants/111288?token=" + trefleKey,
+        minTemp: 5,
+        optimalTemp: 22,
+        maxTemp: 30,
+        minPh: 5.5,
+        maxPh: 6.5,
+        daysToHarvest: 70,
+        plantingMethond: {
+            seed: true,
+            cutting: false,
+            transplant: false,
+        },
+        plantingDensity: {
+            greenhouse: false,
+            field: 30
+        }
+
+    },
+
+    {
+        name: "Lettuce",
+        url: "http://trefle.io/api/plants/147167?token=" + trefleKey,
+        minTemp: 5,
+        optimalTemp: 20,
+        maxTemp: 30,
+        minPh: 5.5,
+        maxPh: 6.5,
+        daysToHarvest: {
+            baby: 28,
+            fullSize: 55,
+        },
+        plantingMethond: {
+            seed: true,
+            cutting: false,
+            transplant: true,
+        },
+        plantingDensity: {
+            greenhouse: 50,
+            field: 25,
+        }
+    },
+
+    {
+        name: "Spinach",
+        url: "http://trefle.io/api/plants/183722?token=" + trefleKey,
+        minTemp: 2,
+        optimalTemp: 18,
+        maxTemp: 25,
+        minPh: 5.5,
+        maxPh: 6.5,
+        daysToHarvest: {
+            baby: 28,
+            fullSize: 55,
+        },
+        plantingMethond: {
+            seed: true,
+            cutting: false,
+            transplant: true,
+        },
+        plantingDensity: {
+            greenhouse: 50,
+            field: 25,
+        }
+    },
+
+
+    // Medium Weather
+
+
+    {
+        name: "Tomato",
+        url: "http://trefle.io/api/plants/182512?token=" + trefleKey,
+        minTemp: 10,
+        optimalTemp: 25,
+        maxTemp: 35,
+        minPh: 6.0,
+        maxPh: 6.5,
+        daysToHarvest: 80,
+        plantingMethond: {
+            seed: true,
+            cutting: true,
+            transplant: true
+        },
+        plantingDensity: {
+            greenhouse: 4,
+            field: 2
+        }
+    },
+
+    {
+        name: "Corn",
+        url: "http://trefle.io/api/plants/193519?token=" + trefleKey,
+        minTemp: 10,
+        optimalTemp: 28,
+        maxTemp: 40,
+        minPh: 6.0,
+        maxPh: 6.5,
+        daysToHarvest: 75,
+        plantingMethond: {
+            seed: true,
+            cutting: false,
+            transplant: false,
+        },
+        plantingDensity: {
+            greenhouse: false,
+            field: 8,
+        }
+    },
+
+    {
+        name: "Peas",
+        url: "http://trefle.io/api/plants/167361?token=" + trefleKey,
+        minTemp: 5,
+        optimalTemp: 22,
+        maxTemp: 30,
+        minPh: 6.0,
+        maxPh: 6.5,
+        daysToHarvest: 75,
+        plantingMethond: {
+            seed: true,
+            cutting: false,
+            transplant: false,
+        },
+        plantingDensity: {
+            greenhouse: false,
+            field: 40,
+        }
+    },
+
+    {
+        name: "Pepper",
+        url: "http://trefle.io/api/plants/115653?token=" + trefleKey,
+        minTemp: 15,
+        optimalTemp: 25,
+        maxTemp: 30,
+        minPh: 6.0,
+        maxPh: 6.5,
+        daysToHarvest: 75,
+        plantingMethond: {
+            seed: true,
+            cutting: true,
+            transplant: true,
+        },
+        plantingDensity: {
+            greenhouse: 4,
+            field: 2,
+        }
+    },
+
+    // Warm Weather
+
+    {
+        name: "Cucumber",
+        url: "http://trefle.io/api/plants/125316?token=" + trefleKey,
+        minTemp: 15,
+        optimalTemp: 28,
+        maxTemp: 40,
+        minPh: 5.5,
+        maxPh: 6.5,
+        daysToHarvest: 52,
+        plantingMethond: {
+            seed: true,
+            cutting: false,
+            transplant: true,
+        },
+        plantingDensity: {
+            greenhouse: 4,
+            field: 2,
+        }
+    },
+
+    {
+        name: "Okra",
+        url: "http://trefle.io/api/plants/101110?token=" + trefleKey,
+        minTemp: 18,
+        optimalTemp: 32,
+        maxTemp: 40,
+        minPh: 5.5,
+        maxPh: 6.5,
+        daysToHarvest: 55,
+        plantingMethond: {
+            seed: true,
+            cutting: false,
+            transplant: true,
+        },
+        plantingDensity: {
+            greenhouse: 4,
+            field: 2,
+        }
+    },
+
+    {
+        name: "Watermelon",
+        url: "http://trefle.io/api/plants/121034?token=" + trefleKey,
+        minTemp: 18,
+        optimalTemp: 32,
+        maxTemp: 40,
+        minPh: 5.5,
+        maxPh: 6.5,
+        daysToHarvest: 80,
+        plantingMethond: {
+            seed: true,
+            cutting: false,
+            transplant: true,
+        },
+        plantingDensity: {
+            greenhouse: false,
+            field: .25,
+        }
+    },
+    {
+        name: "Potato"
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <---------------------------------------------------------------- Max (Line 300-520) ------------------------------------------------------------------------------------>
+
+var vegetables = database.ref().child('Vegetables')
+
+// code for setting data to firebase database. Only needs to be run once
 // for (var i = 0; i < veggies.length; i++) {
 //     vegetables.child(veggies[i].name).set(veggies[i]);
 // }
 
 for (var i = 0; i < veggies.length; i++) {
-    root.ref('Vegetables/' + veggies[i].name).once("value", function (snapshot) {
+    database.ref('Vegetables/' + veggies[i].name).once("value", function (snapshot) {
         console.log(snapshot.val())
+        availableTags.push(snapshot.val().name);
     })
 }
 
 
+// $("submitbutton").on("click", function () {
+    // var plant = $("#plants").val();
+    var plant = "Beets"
+    var daysToHarvest;
+    var maxPh;
+    var minPh;
+    var minReqTempC;
+    var optimalTemp;
+    var plantingField;
+    var plantingGreen;
+    var cutting;
+    var seed;
+    var transplant;
+
+    database.ref('Vegetables/' + plant).once("value", function (snapshot) {
+        daysToHarvest = snapshot.val().daysToHarvest;
+        maxPh = snapshot.val().maxPh;
+        minPh = snapshot.val().minPh;
+        minReqTempC = snapshot.val().minTemp;
+        optimalTemp = snapshot.val().optimalTemp;
+        plantingField = snapshot.val().plantingDensity.field;
+        plantingGreen = snapshot.val().plantingDensity.greenhouse;
+        cutting = snapshot.val().plantingMethod.cutting;
+        seed = snapshot.val().plantingMethod.seed;
+        transplant = snapshot.val().plantingMethod.transplant;
+
+    })
+
+// })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// <--------------------------------------------------------------------------- Halina (Line 300 onward) ------------------------------------------------------------------>
-
-$.ajaxPrefilter(function (options) {
-    if (options.crossDomain && $.support.cors) {
-        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-    }
-});
-
-$.ajax({
-    url: "https://api.awhere.com/oauth/token",
-    type: "POST",
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": "Basic WEVHSTBLUndmY0d5MHJtTXdIck9vR1Y3MlZCR2h0Sng6T1R3RkdERzNuNlBTakdBdw==",
-        'Access-Control-Allow-Origin': '*'
-    },
-    data: "grant_type=client_credentials"
-}).then(function (response) {
-    token = response.access_token;
-    console.log(token);
-
-    startMonth = moment().month();
-    console.log("startMonth " + startMonth);
-
-    //make 4 calls for 1 token
-
-    for (var i = 1; i < 5; i++) {
-
-        //build a mmdd range string for the request parameter from startday to endday
-        var currMoment = moment();
-        console.log(currMoment);
-        startMonth = currMoment.month();
-
-
-        console.log("startMonth " + currMoment.month()); 
-        var futureMonth = moment().add(i, 'months').format('MM');
-
-        //build the month range for the weather api and
-        //store the four month numbers needed for research
-        fourMonths.push(futureMonth);
-        console.log(fourMonths);
-        monthsRange = futureMonth + "-10," + futureMonth + "-20";
-        console.log("mr: " + monthsRange);
-
-        //make API call to get weather data for 10 days of each month
-        $.ajax({
-            url: "https://api.awhere.com/v2/weather/locations/37.7913017,-122.3943565/norms/" + monthsRange + "/years/2015,2018",
-            //url: "https://api.awhere.com/v2/weather/locations/37.7913017,-122.3943565/norms/12-10,12-20/years/2015,2018",
-            //url: "https://api.awhere.com/v2/agronomics/locations/37.7913017,-122.3943565/agronomicnorms/01-01,04-30",
-            method: "GET",
-            headers: {
-                "Authorization": "Bearer " + token
-            }
-        }).then(function (response) {
-            console.log(response);
-
-            for (var j = 0; j < 10; j++) {
-                var wthrDay = {
-                    day: response.norms[j].day,
-                    maxHumidity: response.norms[j].maxHumidity.average,
-                    maxTemp: response.norms[j].maxTemp.average,
-                    meanTemp: response.norms[j].meanTemp.average,
-                    minTemp: response.norms[j].minTemp.average,
-                    precipitation: response.norms[j].precipitation.average
-
-                };
-                console.log(wthrDay);
-
-                //we have to push all the data into one array because the call is asynchronous and there is no order related to months order
-                arrAll.push(wthrDay);
-
-            }
-            console.log(arrAll); //the array is populated
-
-            //call the function separating the array which holds all the data inside 'then' clause because the ajax call is async
-            arrMonthly01.length=0;
-            arrMonthly02.length=0;
-            arrMonthly03.length=0;
-            arrMonthly04.length=0;
-            /*console.log("01 " + arrMonthly01);
-            console.log("02 " + arrMonthly02);
-            console.log("03 " + arrMonthly03);
-            console.log("04 " + arrMonthly04); */
-            
-            separateArray(arrAll);
-
-            console.log(arrMonthly01);
-            console.log(arrMonthly02);
-            console.log(arrMonthly03);
-            console.log(arrMonthly04);  
-
-            //call the function to check if there was a day in the month when a min temperature was below the temperature requirement for the plant
-            //should we use a count to allow this function to run only once
-            //isAllowedToPlantMinTemp(arrMonthly01);
-
-
-
-        });
-        console.log(arrAll); //the array will be empty because of the asynch          
-
-    }    
-  
-});
-
-//arrAll has all 4 months average daily weather data needed and now is the time to analyze it
-//we can separate the array into 4 arrays by months first
-
-function separateArray(arr) {
-
-    console.log("length: " + arr.length)
-    for (var i = 0; i < arr.length; i++) {
-
-        if (arr[i].day.substring(0, 2) == fourMonths[0] && arrMonthly01.length < 10) {
-            //console.log("0 day " + [i] + " " +  arr[i].day.substring(0, 2))
-            arrMonthly01.push(arr[i])
-        }
-        else if (arr[i].day.substring(0, 2) == fourMonths[1] && arrMonthly02.length < 10) {
-            //console.log("1 day " + [i] + " " +  arr[i].day.substring(0, 2))
-            arrMonthly02.push(arr[i])
-        }
-        else if (arr[i].day.substring(0, 2) == fourMonths[2] && arrMonthly03.length < 10) {
-            //console.log("2 day " + [i] + " " +  arr[i].day.substring(0, 2))
-            arrMonthly03.push(arr[i])
-        }
-        else if (arr[i].day.substring(0, 2) == fourMonths[3] && arrMonthly04.length < 10) {
-            //console.log("3 day " + [i] + " " +  arr[i].day.substring(0, 2))
-            arrMonthly04.push(arr[i])
-        }
-    }
-
-}
-
-//this function returns true if daily min temeratures within a month were higher than the min required temperature for a plan 
-//returns false otherwise
-function isAllowedToPlantMinTemp (arr, minReqTemp) {
-
-    for (var i = 0; i <= arr.length; i++) {
-        if (arr[i].minTemp <=  minReqTemp) {
-            return false;
-        }
-
-    }
-
-    return true;
-
-}
-
-
-
-
-        //read the plant data from the firebase
-
-        //check if the min temperature for each month data is not higher than the min tempearture required by a plant
-        //for this run 4 loops for each month and check minTemp property for comparison
-
-
-        //accumulate average for meanTemp.average for each month?
-
-        //accumulate average for maxTemp.average for each month?
